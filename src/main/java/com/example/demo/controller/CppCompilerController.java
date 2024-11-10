3d33745111226e0ceef97533d780362c41efb186
Roby15 <trifasrobert5@gmail.com>
2024-11-10 10:43:50 +0200

    Cors

--- src/main/java/com/example/demo/controller/CppCompilerController.java
+++ src/main/java/com/example/demo/controller/CppCompilerController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/compile")
+@CrossOrigin(origins = "*") // Allow all origins
 public class CppCompilerController {
 
     @Autowired
